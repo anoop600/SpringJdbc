0d9323368b7255d4bbd691a595aadb09428cf2aa
Anoop Jain <anoop.jain10@gmail.com>
2018-12-28 14:14:12 +0530

    Update App.java

--- src/main/java/com/mindtree/springJdbcDemo/App.java
+++ src/main/java/com/mindtree/springJdbcDemo/App.java
@@ -12,7 +12,7 @@ import com.mindtree.springJdbcDemo.service.serviceImpl.EmployeeServiceImpl;
 
 public class App {
 	public static void main(String[] args) {
-		 EmployeeService employeeService = new EmployeeServiceImpl();
+		// EmployeeService employeeService = new EmployeeServiceImpl();
 		ApplicationContext contextEntity = new ClassPathXmlApplicationContext(
 				"com\\mindtree\\springJdbcDemo\\util\\EntityBean.xml");
 
@@ -21,38 +21,49 @@ public class App {
 
 		EmployeeService employeeService = contextService.getBean("service", EmployeeServiceImpl.class);
 
-		Employee emp = contextEntity.getBean("employee", Employee.class);
+		for (int i = 0; i < 10; i++) {
+			Employee emp = contextEntity.getBean("employee", Employee.class);
+			emp.setUsername("Name"+i);
+			emp.setPassword("Password"+i);
+			emp.setFullName("FullName"+i);
+			emp.setDob("Date"+i);
+			emp.setEmailid(emp.getFullName()+"@gmail.com");
+			emp.setGender("Male");
+			emp.setSecurityQuestion("Question "+i);
+			emp.setSecurityAnswer("Answer "+i);
+			employeeService.create(emp);
+		}
 
 		// Insert into Employee
 
-		emp.setUsername("idujhcwhec");
-		emp.setPassword("aj123".hashCode() + "");
-		emp.setFullName("Anoop"); // emp.setDob(Date.valueOf(s.next()) + "");
-		emp.setDob("1995-12-08");
-		emp.setEmailid("a@a");
-		emp.setGender("male");
-		emp.setSecurityQuestion("Qest");
-		emp.setSecurityAnswer("Ans");
+//		emp.setUsername("idujhcwhec1");
+//		emp.setPassword("idujhcwhec1".hashCode() + "");
+//		emp.setFullName("idujhcwhec1"); // emp.setDob(Date.valueOf(s.next()) + "");
+//		emp.setDob("1995-12-08");
+//		emp.setEmailid("idujhcw1hec");
+//		emp.setGender("idujhcwh1ec");
+//		emp.setSecurityQuestion("i1dujhcwhec");
+//		emp.setSecurityAnswer("idu1jhcwhec");
 
-		 System.out.println(emp);
+		// System.out.println(emp);
 
-		employeeService.create(emp);
+		
 
 		// Read//
-		 System.out.println(employeeService.read(10));
+		// System.out.println(employeeService.read(10));
 
 		// Delete
 
-		 System.out.println(employeeService.delete(5));
+		// System.out.println(employeeService.delete(5));
 
 		// Get all
-		 employeeService.getAllEmployee().forEach(System.out::println);
+		employeeService.getAllEmployee().forEach(System.out::println);
 
 		// Update
 
-		
-		 Employee employee = new Employee(); employee.setEmpId(12);
-		 employee.setEmailid("hi@hi"); employeeService.update(employee);
-		
+		/*
+		 * Employee employee = new Employee(); employee.setEmpId(12);
+		 * employee.setEmailid("hi@hi"); employeeService.update(employee);
+		 */
 	}
 }
